fb4778c0846310f3863be5658da82c70a7f3866d
ultom <ultom@DESKTOP-HHJ2Q5T>
2023-01-19 21:14:30 -0500

    Week13-14 lab

---
+++ src/main/java/twoSum.java
@@ -0,0 +1,28 @@
+
+//Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+//You may assume that each input would have exactly one solution, and you may not use the same element twice.
+//You can return the answer in any order.
+
+
+
+
+public class twoSum {
+    
+  int[] myNum = {2,7,11,15};
+  int target = 9;
+  int tempValue = 0;
+  
+  
+ 
+public void main(String[] args){
+    if (tempValue == target) {
+        
+        
+        
+    }
+    
+  }
+    
+}
+
+
